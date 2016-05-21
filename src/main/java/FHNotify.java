5504fa07eccdfd7db15df028159a82dd8149bc9a
chargi <chargi@gmail.com>
2016-05-21 12:01:34 +0200

    Arg handling

--- src/main/java/FHNotify.java
+++ src/main/java/FHNotify.java
@@ -8,6 +8,8 @@ import classes.strategy.Whatsapp;
 import interfaces.Alias;
 import interfaces.MessageText;
 import interfaces.Strategy;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,9 +28,22 @@ public class FHNotify {
         List<String> formatIdentifiers = new ArrayList<>();
         String strategy;
 
+        //Mockup args
+        String[] mockArgs = {"--config", "~/test.json", "--message", "test.txt", "--format", "casual-greeting,formal-greeting,christmas", "--transport", "whatsapp" };
+
         //TODO: OptionParser
-        //OptionParser parser = new OptionParser( "a:" );
-        //OptionSet options = parser.parse( "-a", "foo", "-abar", "-a=baz" );
+        OptionParser parser = new OptionParser();
+        parser.accepts("config", "Declare config file").withOptionalArg().ofType(String.class);
+        parser.accepts("message", "Declare message file").withRequiredArg().ofType(String.class);
+        parser.accepts("format", "Set text formatters").withOptionalArg();
+        parser.accepts("transport", "Declare way of transport").withRequiredArg().ofType(String.class);
+        OptionSet options = parser.parse(mockArgs);
+        String configOption = (String) options.valueOf("config");
+        String configMessage = (String) options.valueOf("message");
+        String configFormat = (String) options.valueOf("format");
+        String configTransport = (String) options.valueOf("transport");
+        String[] configFormats = configFormat.split(",");
+
 
         /* COMPOSITION
         * Config File einlesen
